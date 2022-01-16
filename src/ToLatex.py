c6630e7ec1dcf5d7ca2fca2da88001da63f629d7
LiGuer <2975395623@qq.com>
2022-01-16 23:23:24 +0800

    Update

--- src/ToLatex.py
+++ src/ToLatex.py
@@ -1,5 +1,22 @@
 import re
 
+if __name__ == '__main__':
+    fileName = "信息论.tex"
+    file = open(fileName,"r", encoding='utf-8')
+    Str = file.read()
+    file.close()
+
+    Str = ToLatex(Str)
+
+    file = open("D:/矩阵论.tex","w+", encoding='utf-8')
+    file.write(Str)
+    file.close()
+
+def Cut(Str):
+    Str = re.sub(r'\n {' + str(int(4 * 3)) + r',}[^\n]*', r'', Str)
+    Str = re.sub(r'\n\s*\n', r'\n', Str)
+    return Str
+
 def Item(code):
     while(1):
         # a
@@ -35,31 +52,37 @@ def Item(code):
         code = codeA + codeC + code[c+1:]
     return code
 
-if __name__ == '__main__':
-    fileName = "信息论.tex"
-
-    file = open(fileName,"r", encoding='utf-8')
-    Str = file.read()
+def ToLatex(Str):
+    #Str = re.sub(r'\n\s*\n', r'\n', Str)
+    #Str = re.sub(r'(\n {4*1,})\n', r'\n', Str)
     # 简单命令
     Str = re.sub(r'\\\.', r'\\boldsymbol', Str)
     Str = re.sub(r'\\bf', r'\\textbf', Str)
-    Str = re.sub(r'\\def', r'\\defi', Str)
+    Str = re.sub(r'\\Example', r'\\textbf{例子}', Str)
+    Str = re.sub(r'\\Property', r'\\textbf{性质}', Str)
+    Str = re.sub(r'\\Proof', r'\\textbf{证明}', Str)
+    Str = re.sub(r'\\def\{([\S]+)\}', lambda m: '\\textbf{定义(' + m.group(1)+ ')}', Str)
+    Str = re.sub(r'\\Theorem\{([\S]+)\}', lambda m: '\\textbf{定理(' + m.group(1)+ ')}', Str)
     # section
-    Str = re.sub(r'\\section ([\s\S][^\n]+)\n', lambda m:'\\section{' + m.group(1) + '}\n', Str)
-    Str = re.sub(r'(\n {8})\\section', lambda m:m.group(1) +'\\subsubsection', Str)
-    Str = re.sub(r'(\n {4})\\section', lambda m:m.group(1) +'\\subsection', Str)
-    Str = re.sub(r'(\n +)\\section', lambda m:m.group(1) +'\\noindent\\textbf', Str)
+    Str = re.sub(r'(\n {8})\*', lambda m:m.group(1) +'\\subsubsection', Str)
+    Str = re.sub(r'(\n {4})\*', lambda m:m.group(1) +'\\subsection', Str)
+    Str = re.sub(r'\n\*', r'\n\\section', Str)
+    Str = re.sub(r'section ([\s\S][^\n]+)\n', lambda m:'section{' + m.group(1) + '}\n', Str)
     # 列表
     Str = Item(Str)
     # 公式
+    Str = re.sub(r'≥', r'\\ge', Str)
+    Str = re.sub(r'≤', r'\\le', Str)
+    Str = re.sub(r'∂', r'\\partial ', Str)
+    Str = re.sub(r' => ', r' \\Rightarrow ', Str)
+    Str = re.sub(r' <=> ', r' \\Leftrightarrow ', Str)
+    Str = re.sub(r'\\sum_', r'\\sum\\limits_', Str)
+    Str = re.sub(r'\\prod_', r'\\prod\\limits_', Str)
+
     Str = re.sub(r'\n(\s+)\$([\s\S][^\$]+)\$\n', lambda m:'\n'+ m.group(1) + '\\begin{align*}'+m.group(2)+'\\end{align*}\n', Str)
 
     file.close()
     file = open("head.tex","r", encoding='utf-8')
     Str = file.read() + Str + "\n\\end{document}"
-    #print(Str)
 
-    file.close()
-    file = open("D:/矩阵论.tex","w+", encoding='utf-8')
-    file.write(Str)
-    file.close()
+    return Str
